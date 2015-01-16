aec96057bc1478371d1b66f535cedcc7afe1c790
dhstuart <dhstuart@users.noreply.github.com>
2015-01-15 16:55:13 -0800

    initial

---
+++ Functions/metadata_output_for_DAQ.m
@@ -0,0 +1,23 @@
+clear all
+close all
+clc
+
+
+% addpath Library\export_fig
+% addpath Library\export_fig\.ignore
+% cd('photometric data')
+cd ..
+load Data\LEDLifeTestingData2.mat
+columns = [1:2 4:9];
+names = fieldnames(data(1,1));
+for i = 1:size(data,1)
+    for j = 1:size(data,2)  
+        temp = struct2cell(data(i,j));
+        index = (i-1)*size(data,2)+j
+        a(index,:) = temp(columns)';
+        
+    end
+end
+
+tableOut = [names(columns)';a];
+cell2csv('LedLifeTestingMetadata.csv',tableOut)
